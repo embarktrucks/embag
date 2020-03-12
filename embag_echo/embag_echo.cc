4be663b74d4b119147102c1acf7ecd0d53198dad
Jason Snell <jason@embarktrucks.com>
2020-03-12 13:30:18 -0700

    WIP

--- embag_echo/embag_echo.cc
+++ embag_echo/embag_echo.cc
@@ -18,8 +18,15 @@ int main(int argc, char *argv[]) {
 
   for (const auto &message : reader.getView().getMessages("/CANMessageIn/vcan")) {
     std::cout << message->topic << " at " << message->timestamp.secs << "." << message->timestamp.nsecs << std::endl;
+    // TODO: string_value make private
     std::cout << message->data->get("messages")->get(0)->get("name")->string_value << std::endl;
     std::cout << message->data->get("messages")->get(0)->get("name")->get<std::string>() << std::endl;
+
+    // TODO: this is the target api, implement this!
+    std::cout << message->data("messages")->at(0)->getValue<std::string>("name") << std::endl;
+
+    message("messages")
+    message->data("messages")
     std::cout << "----------------------------" << std::endl;
   }
 
