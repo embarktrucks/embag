4d8c42f3fc02a575d0b3379a094a028c3b5692e6
Jason Snell <jason@embarktrucks.com>
2020-10-04 16:40:02 -0400

    Add wheel package.

--- pip_package/macos_build.sh
+++ pip_package/macos_build.sh
@@ -6,11 +6,11 @@ cp -r pip_package/* README.md LICENSE /tmp/pip_build
 
 # Build embag libs and echo test binary
 bazel build //python:libembag.so //embag_echo:embag_echo && \
-
 bazel test test:* --test_output=all
 
 # Build wheel
 cp bazel-bin/python/libembag.so /tmp/pip_build/embag
+python -m pip install wheel
 (cd /tmp/pip_build && python setup.py bdist_wheel && \
  python -m pip install dist/embag*.whl && \
  python -c 'import embag; embag.View(); print("Successfully loaded embag!")' &&\
