721b89d5fff18f93fb352f4d01f979cc2f7a1c0d
Jason Snell <jason@embarktrucks.com>
2020-10-04 11:10:02 -0400

    Removing version switching to handle it with the tool cache.

--- pip_package/macos_build.sh
+++ pip_package/macos_build.sh
@@ -1,25 +1,19 @@
 #!/bin/bash -e
 
-mkdir /tmp/embag /tmp/pip_build /tmp/out
+mkdir -p /tmp/embag /tmp/pip_build /tmp/out
 cp -r lib /tmp/embag
 cp -r pip_package/* README.md LICENSE /tmp/pip_build
 
-# Build embag for various version of Python
-for version in 3.5 3.6 3.7 3.8
-  do
-    # Build embag libs and echo test binary
-    PYTHON_PATH=`which python$version`
-    PYTHON_BIN_PATH=$PYTHON_PATH bazel build //python:libembag.so //embag_echo:embag_echo && \
+# Build embag libs and echo test binary
+bazel build //python:libembag.so //embag_echo:embag_echo && \
 
-    # FIXME: This may not run with the correct version of python...
-    bazel test test:* --test_output=all
+bazel test test:* --test_output=all
 
-    # Build wheel
-    cp bazel-bin/python/libembag.so /tmp/pip_build/embag
-    (cd /tmp/pip_build && $PYTHON_PATH setup.py bdist_wheel && \
-     $PYTHON_PATH -m pip install dist/embag*.whl && \
-     $PYTHON_PATH -c 'import embag; embag.View(); print("Successfully loaded embag!")' &&\
-     cp dist/* /tmp/out && \
-     rm -rf build dist)
-  done
+# Build wheel
+cp bazel-bin/python/libembag.so /tmp/pip_build/embag
+(cd /tmp/pip_build && $PYTHON_PATH setup.py bdist_wheel && \
+ python -m pip install dist/embag*.whl && \
+ python -c 'import embag; embag.View(); print("Successfully loaded embag!")' &&\
+ cp dist/* /tmp/out && \
+ rm -rf build dist)
 
