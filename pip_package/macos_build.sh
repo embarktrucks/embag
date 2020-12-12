466f6086236a5bbc1ecf0c5c95baaa7000b9631c
Jason Snell <jason@embarktrucks.com>
2020-12-11 21:44:48 -0800

    Still trying...

--- pip_package/macos_build.sh
+++ pip_package/macos_build.sh
@@ -5,15 +5,15 @@ cp -r lib /tmp/embag
 cp -r pip_package/* README.md LICENSE /tmp/pip_build
 
 # Build embag libs and echo test binary
-bazel build //python:libembag.so //embag_echo:embag_echo && \
-bazel test //test:embag_test //test:embag_test_python3 --test_output=all
+PYTHON_BIN_PATH=/usr/bin/python3 bazel build //python:libembag.so //embag_echo:embag_echo && \
+PYTHON_BIN_PATH=/usr/bin/python3 bazel test //test:embag_test //test:embag_test_python3 --test_output=all
 
 # Build wheel
 cp bazel-bin/python/libembag.so /tmp/pip_build/embag
-python -m pip install wheel
-(cd /tmp/pip_build && MACOSX_DEPLOYMENT_TARGET=11.0 python setup.py bdist_wheel && \
- MACOSX_DEPLOYMENT_TARGET=11.0 python -m pip install dist/embag*.whl && \
- python -c 'import embag; embag.View(); print("Successfully loaded embag!")' &&\
+python3 -m pip install wheel
+(cd /tmp/pip_build && python3 setup.py bdist_wheel && \
+ python3 -m pip install dist/embag*.whl && \
+ python3 -c 'import embag; embag.View(); print("Successfully loaded embag!")' &&\
  cp dist/* /tmp/out && \
  rm -rf build dist)
 
