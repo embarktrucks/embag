8c9bc7d58c6f91b09c79aa7ed33ae760780e189d
Daniel feldman <def@applied.co>
2021-11-15 16:58:12 -0800

    add check for cpp version in lib/util.h

--- lib/util.h
+++ lib/util.h
@@ -3,7 +3,7 @@
 #include <boost/iostreams/stream.hpp>
 
 namespace Embag {
-
+#if __cplusplus < 201402L
 // std::make_unique is not available in c++11 :(
 template<typename T, typename... Args>
 std::unique_ptr<T> make_unique(Args &&... args) {
@@ -11,4 +11,7 @@ std::unique_ptr<T> make_unique(Args &&... args) {
 }
 
 using message_stream = boost::iostreams::stream<boost::iostreams::array_source>;
+#else
+  using std::make_unique;
+#endif
 }
