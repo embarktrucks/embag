f33221b3c37d5f340e33682ccfdfc0b5f8563047
liamembark <liam@embarktrucks.com>
2021-12-03 15:11:50 -0800

    Fix python view iteration

--- python/ros_compat.h
+++ python/ros_compat.h
@@ -12,8 +12,12 @@ struct IteratorCompat {
 
   py::tuple operator*() const {
     const auto msg = *iterator_;
-    // TODO: Change the timestamp to a rostime - they have more precision!
-    return py::make_tuple(msg->topic, msg->data(), msg->timestamp.to_sec());
+    return py::make_tuple(
+      msg->topic,
+      (Embag::VectorItemPointer<Embag::RosValue>&) msg->data(),
+      // TODO: Change the timestamp to a rostime - they have more precision!
+      msg->timestamp.to_sec()
+    );
   }
 
   bool operator==(const IteratorCompat &other) const {
