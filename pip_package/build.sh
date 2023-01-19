3eab45e9e393ea8a98406ddecf69a6418956de28
Sanjeevani Lakshmivarahan <sanjeevani.lakshmivarahan@embarktrucks.com>
2023-01-19 14:53:40 -0800

    Fix order of user argument and python path

--- pip_package/build.sh
+++ pip_package/build.sh
@@ -1,25 +1,27 @@
 #!/bin/bash -e
 
 function build() {
-  PYTHON_PATH=$1
+  PYTHON_PARENT_FOLDER=$1
   PYTHON_VERSION=$2
 
+  PYTHON_PATH="$PYTHON_PARENT_FOLDER/python$PYTHON_VERSION"
+
   # Install necessary dependencies
-  # "$PYTHON_PATH/pip3" install --user -r /tmp/pip_build/requirements.txt
-  "$PYTHON_PATH/python$PYTHON_VERSION" -m pip install --user -r /tmp/pip_build/requirements.txt
+  # "$PYTHON_PARENT_FOLDER/pip3" install --user -r /tmp/pip_build/requirements.txt
+  "$PYTHON_PATH" -m pip install -r /tmp/pip_build/requirements.txt --user
 
   # Build embag libs and echo test binary
   (cd /tmp/embag &&
-    PYTHON_BIN_PATH="$PYTHON_PATH/python" bazel build -c opt //python:libembag.so //embag_echo:embag_echo &&
-    PYTHON_BIN_PATH="$PYTHON_PATH/python" bazel test -c opt //test:embag_test "//test:embag_test_python$PYTHON_VERSION" --cache_test_results=no --test_output=all)
+    PYTHON_BIN_PATH="$PYTHON_PATH" bazel build -c opt //python:libembag.so //embag_echo:embag_echo &&
+    PYTHON_BIN_PATH="$PYTHON_PATH" bazel test -c opt //test:embag_test "//test:embag_test_python$PYTHON_VERSION" --cache_test_results=no --test_output=all)
 
   # Build wheel
   cp /tmp/embag/bazel-bin/python/libembag.so /tmp/pip_build/embag
-  (cd /tmp/pip_build && "$PYTHON_PATH/python" setup.py bdist_wheel &&
+  (cd /tmp/pip_build && "$PYTHON_PATH" setup.py bdist_wheel &&
     auditwheel repair /tmp/pip_build/dist/embag*.whl --plat manylinux2014_x86_64 &&
-    # "$PYTHON_PATH/pip" install wheelhouse/embag*.whl &&
-    "$PYTHON_PATH/python$PYTHON_VERSION" -m pip install wheelhouse/embag*.whl &&
-    "$PYTHON_PATH/python$PYTHON_VERSION" -c 'import embag; embag.View(); print("Successfully loaded embag!")' &&
+    # "$PYTHON_PARENT_FOLDER/pip" install wheelhouse/embag*.whl &&
+    "$PYTHON_PATH" -m pip install wheelhouse/embag*.whl --user &&
+    "$PYTHON_PATH" -c 'import embag; embag.View(); print("Successfully loaded embag!")' &&
     cp wheelhouse/* /tmp/out &&
     rm wheelhouse/* &&
     rm -rf build dist)
